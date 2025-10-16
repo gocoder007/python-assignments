925869f3dbbe3b3f067a305c00afae519ceefb66
gocoder007 <conqueror8367@gmail.com>
2025-10-16 21:14:55 +0530

    Added Task 1 and Task 2 Python scripts

---
+++ Task 1 Perform Basic Mathematical Operations.py
@@ -0,0 +1,12 @@
+#simple calculator
+N1 = float(input('Enter the first number: '))
+N2 = float(input('Enter the second number: '))
+
+print()
+
+print('Addition: ', N1+N2)
+print('Subtraction: ', N1-N2)
+print('Multiplication: ', N1*N2)
+print('Division: ', N1/N2)
+
+
---
+++ Task 2 Create a Personalized Greeting.py
@@ -0,0 +1,10 @@
+N1 = input ('Enter the first name: ')
+N2 = input ('Enter the last name: ')
+
+print()
+
+print('Hello, ' + N1 + ' ' + N2 + ' ' + 'welcome to the python program')
+
+
+
+
